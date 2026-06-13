9a951e610aeadc5b47db3eb2879fabbb84910fce
Malaika Naveed <malaikanaveed26@gmail.com>
2026-06-13 12:57:43 +0500

    Update OCR route to use new controller

--- src/routes/ocr.js
+++ src/routes/ocr.js
@@ -1,8 +1,8 @@
 import express from "express"
 import multer from "multer"
 import { authenticateToken } from '../middlewares/jwt.js';
-import { extractMedicalTerms } from "../controllers/medicalHistoryController.js";
-const router=express.Router();
-const upload=multer();
+import { extractMedicalTerms } from "../controllers/ocrController.js";
+const router = express.Router();
+const upload = multer();
 router.post("/ocr", upload.single("image"), extractMedicalTerms);
 export default router;
