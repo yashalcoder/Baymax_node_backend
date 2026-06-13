5d24486199844b138d247518fa8d1465ca98e36e
Malaika Naveed <malaikanaveed26@gmail.com>
2026-06-13 12:29:20 +0500

    Simplify handler to return a hello message

--- api/index.js
+++ api/index.js
@@ -1,20 +1,3 @@
-import dotenv from "dotenv";
-import connectdb from "../src/config/db.js";
-import app from "../src/app.js";
-
-dotenv.config();
-
-let isConnected = false;
-
-export default async function handler(req, res) {
-  if (!isConnected) {
-    try {
-      await connectdb();
-      isConnected = true;
-    } catch (err) {
-      console.error("DB connection failed:", err);
-      return res.status(500).json({ message: "DB connection failed", error: err.message });
-    }
-  }
-  return app(req, res);
+export default function handler(req, res) {
+  res.status(200).json({ message: "hello" });
 }
